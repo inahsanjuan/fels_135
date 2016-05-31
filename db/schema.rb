c3bacfd2ce72653fa3473cbe32d2d4e0ca52254c
Vu Dinh Hao <vudinhhao.framgia.com>
2016-05-31 09:14:28 +0700

    Static-Pages

--- db/schema.rb
+++ db/schema.rb
@@ -37,22 +37,26 @@ ActiveRecord::Schema.define(version: 20160530083137) do
   add_index "lesson_words", ["word_id"], name: "index_lesson_words_on_word_id"
 
   create_table "lessons", force: :cascade do |t|
-    t.boolean  "check_lesson"
+    t.boolean  "status"
     t.integer  "user_id"
     t.integer  "category_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "lessons", ["category_id"], name: "index_lessons_on_category_id"
   add_index "lessons", ["user_id"], name: "index_lessons_on_user_id"
 
   create_table "relationships", force: :cascade do |t|
-    t.integer  "fllower_id"
+    t.integer  "follower_id"
     t.integer  "followed_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
