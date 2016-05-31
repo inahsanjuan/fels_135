f3442bc9cd67d31aa5cf77fa97cbdd1c0c51d42b
tuyenmary <tuyenmary@gmail.com>
2016-05-31 18:43:25 +0700

    edit-update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
+
   def new
   end
 
   def show
-    @user = User.find params[:id]
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update_attributes user_params
+      flash[:success ] = t "profileupdated"
+      redirect_to current_user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
