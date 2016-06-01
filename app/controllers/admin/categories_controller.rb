d36cbf3130d05e01051b870ae7377add2f7ee468
tuyenmary <tuyenmary@gmail.com>
2016-06-01 15:31:33 +0700

    admin-edit-categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,33 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :correct_admin, only: [:edit, :update]
+  before_action :load_category, only: [:show, :edit, :update]
   def index
     @categories = Category.paginate page: params[:page]
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = I18n.t "admin.categoryupdated"
+      redirect_to admin_category_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def load_category
+    @category = Category.find params[:id]
+  end
+
+  def category_params
+    params.require(:category).permit :name
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  def correct_admin
+    unless current_user.admin?
+      flash[:danger] = I18n.t "admin.correct"
+      redirect_to root_url
+    end
+  end
 end
