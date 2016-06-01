46b5e26f7e386914aa80c2171c20ae5189d71ed0
Vu Dinh Hao <vudinhhao.framgia.com>
2016-06-01 11:37:17 +0700

    add-categories

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def index
     @user = User.all
   end
 
   def show
+    @user = User.find params[:id]
   end
 
   def new
@@ -36,4 +39,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
   end
+
 end
